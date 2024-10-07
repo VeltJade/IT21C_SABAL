32cf04c6250eefc611c4b695cef62ba3c6493003
VeltJade <judeyasabal@gmail.com>
2024-10-07 23:21:11 +0800

    Objects and Classes

---
+++ topic-1.js
@@ -0,0 +1,8 @@
+const person1={
+    name: "Velt Jade Sabal",
+    age: 21,
+    occupation: "Model"
+}
+console.log("Name: " + person1.name);
+console.log("Age: " + person1.age);
+console.log("Occupation: " + person1.occupation);
